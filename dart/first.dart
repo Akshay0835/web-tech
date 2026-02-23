2c8e13cea32d35a30cda73070176e75bc76cf45b
Akshay <akshaypratapsingh4219@gmail.com>
2026-02-23 12:19:05 +0530

    dart 2

--- dart/first.dart
+++ dart/first.dart
@@ -17,12 +17,44 @@
 //   athlete.run();
 // }
 
-Future<String> fetchData() {
-  return Future.delayed(Duration(seconds: 2), () => "Data Loaded");
+// Future<String> fetchData() {
+//   return Future.delayed(Duration(seconds: 2), () => "Data Loaded");
+// }
+
+// void main() async {
+//   print("Loading.....");
+//   String result = await fetchData();
+//   print(result);
+// }
+
+// Create a Vehicle Class inherit a Car class that adds a property like model or speed
+//Write a method that prints basic vehicle info and car info
+
+class Vehicle {
+  String brand;
+
+  Vehicle(this.brand);
+
+  void printInfo() {
+    print("Vehicle Brand: $brand");
+  }
 }
 
-void main() async {
-  print("Loading.....");
-  String result = await fetchData();
-  print(result);
+class Car extends Vehicle {
+  String model;
+  int speed;
+
+  Car(String brand, this.model, this.speed) : super(brand);
+
+  @override
+  void printInfo() {
+    super.printInfo();
+    print("Car Model: $model");
+    print("Car Speed: $speed km/h");
+  }
+}
+
+void main() {
+  Car myCar = Car("Toyota", "Corolla", 180);
+  myCar.printInfo();
 }
